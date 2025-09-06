e3fef849b10550dc82b581df836568373d90eaa5
josue173 <josue.perez04@outlook.com>
2025-09-06 12:44:25 -0600

    fix: arreglos menores

--- Inventario.Client/Program.cs
+++ Inventario.Client/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Inventario.Client;
 using Inventario.Client.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
